2c23ffc1170d2186fbc142ef4d562f90e30dbce7
vanh2001 <anhviet1709@gmail.com>
2022-02-09 15:24:28 +0700

    Add bai tim GTLN cua mang

---
+++ find_max_element/src/Main.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        //B1: Khai báo các biến, nhập và kiểm tra kích thước mảng
+        int size;
+        int[] array;
+        Scanner scanner = new Scanner(System.in);
+        do {
+            System.out.print("Enter a size:");
+            size = scanner.nextInt();
+            if (size > 20)
+                System.out.println("Size should not exceed 20");
+        } while (size > 20);
+        //B2: Nhập giá trị cho các phần tử của mảng
+        array = new int[size];
+        int i = 0;
+        while (i < array.length) {
+            System.out.print("Enter element" + (i + 1) + " : ");
+            array[i] = scanner.nextInt();
+            i++;
+        }
+        //B3: In ra danh sách tài sản đã nhập
+        System.out.print("Property list: ");
+        for (int j = 0; j < array.length; j++) {
+            System.out.print(array[j] + "\t");
+        }
+        //B4: Duyệt các phần tử trong mảng để tìm GTLN và vị trí
+        int max = array[0];
+        int index = 1;
+        for (int j = 0; j < array.length; j++) {
+            if (array[j] > max) {
+                max = array[j];
+                index = j + 1;
+            }
+        }
+        System.out.println("The largest property value in the list is " + max + " ,at position " + index);
+    }
+
+}
