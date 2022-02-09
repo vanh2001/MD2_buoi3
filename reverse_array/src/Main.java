21ce9114b93c48290ad4357b9ec2e667d1d6e19d
vanh2001 <anhviet1709@gmail.com>
2022-02-09 23:13:55 +0700

    Update bai dao nguoc mang

--- reverse_array/src/Main.java
+++ reverse_array/src/Main.java
@@ -2,35 +2,40 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+//        B1: Khai báo các biến, nhập và kiểm tra kích thước mảng
         int size;
-        int[] array;
-        Scanner scanner = new Scanner(System.in);
+        int array[];
+        Scanner sc = new Scanner(System.in);
         do {
-            System.out.print("Enter a size:");
-            size = scanner.nextInt();
-            if (size > 20)
+            System.out.print("Enter a size: ");
+            size = sc.nextInt();
+            if (size > 20){
                 System.out.println("Size does not exceed 20");
-        } while (size > 20);
+            }
+        }while (size > 20);
+//        B2: Nhập giá trị cho các phần tử của mảng
         array = new int[size];
         int i = 0;
-        while (i < array.length) {
+        while (i < array.length){
             System.out.print("Enter element " + (i + 1) + ": ");
-            array[i] = scanner.nextInt();
+            array[i] = sc.nextInt();
             i++;
         }
-        System.out.printf("%-20s%s", "Elements in array: ", "");
+//        B3: In ra mảng đã nhập
+        System.out.printf("%-20s%s", "Element in array: ", "");
         for (int j = 0; j < array.length; j++) {
-            System.out.print(array[j] + "\t");
+            System.out.print(array[j] + "/t");
         }
+//        B4: Đảo ngược thứ tự các phần tử trong mảng
         for (int j = 0; j < array.length / 2; j++) {
             int temp = array[j];
             array[j] = array[size - 1 - j];
             array[size - 1 - j] = temp;
         }
+//        B5: In ra mảng sau khi đã đảo ngược thứ tự các phần tử
         System.out.printf("\n%-20s%s", "Reverse array: ", "");
         for (int j = 0; j < array.length; j++) {
             System.out.print(array[j] + "\t");
         }
-
     }
 }
